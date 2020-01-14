04d120c7dd660befb2f9bf898b1076681b265525
Thomson19 <tomek.siekacz97@gmail.com>
2020-01-14 09:17:55 +0100

    1.5.6 (poprawka) dodanie dokumentacji do klasy IntLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,10 +1,16 @@
 package edu.kis.vh.nursery.list;
-
+/**
+* Simple class represents linked list. It can contain integers.
+* */
 public class IntLinkedList {
 
     private Node last;
     public static final int EMPTY_LIST = -1;
 
+    /**
+     * Add element to the end of the list.
+     * @param i element
+     * */
     public void push(int i) {
         if (getLast() == null)
             setLast(new Node(i));
@@ -14,21 +20,33 @@ public class IntLinkedList {
             setLast(getLast().getNext());
         }
     }
-
+    /**
+     * Check if list is empty.
+     * Returns true if list is empty, else returns false
+     * */
     public boolean isEmpty() {
         return getLast() == null;
     }
-
+    /**
+     * Check if list is full.
+     * Returns true if list is full else returns false
+     * */
     public boolean isFull() {
         return false;
     }
-
+    /**
+     * Take last element from list without deleting it.
+     * Returns top element
+     * */
     public int top() {
         if (isEmpty())
             return EMPTY_LIST;
         return getLast().getValue();
     }
-
+    /**
+     * Delete and return last element from list.
+     * If list is empty returns -1
+     * */
     public int pop() {
         if (isEmpty())
             return EMPTY_LIST;
@@ -36,11 +54,16 @@ public class IntLinkedList {
         setLast(getLast().getPrev());
         return ret;
     }
-
+    /**
+     * Returns last node from list.
+     * */
     public Node getLast() {
         return last;
     }
 
+    /**
+     * Set last node.
+     * */
     public void setLast(Node last) {
         this.last = last;
     }
